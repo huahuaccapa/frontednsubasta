4ddbe6e08cca1d5ef0c9ce76422771b043573345
huahuaccapa <huahuaccapa1119@gmail.com>
2025-07-17 01:22:36 -0500

    Updated oauthService.js file

--- app/Services/oauthService.js
+++ app/Services/oauthService.js
@@ -1,24 +1,15 @@
 // app/Services/oauthService.js
 import apiClient from '../Axios/axios';
 
-const OAUTH_ENDPOINT = '/oauth2'; // Esto se concatenará con la baseURL del apiClient
-
-export const getUserInfo = async () => {
-  try {
-    const response = await apiClient.get(`${OAUTH_ENDPOINT}/user-info`);
-    return response.data;
-  } catch (error) {
-    console.error('Error fetching user info:', error);
-    throw error;
-  }
+export const handleOAuthRedirect = () => {
+  window.location.href = `${process.env.NEXT_PUBLIC_API_BASE_URL}/oauth2/authorize/google`;
 };
 
 export const oauthSuccess = async () => {
   try {
-    const response = await apiClient.get(`${OAUTH_ENDPOINT}/success`);
+    const response = await apiClient.get('/oauth2/success');
     return response.data;
   } catch (error) {
-    console.error('Error handling OAuth success:', error);
     throw error;
   }
 };
